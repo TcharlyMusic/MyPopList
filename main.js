15b3d71fe9f25083320ebec915bba12880cb3da9
TcharlyMusic <duprezjc@gmail.com>
2024-11-08 19:09:15 +0100

    Update DATAS.JSON with State and few seclect options

--- main.js
+++ main.js
@@ -18,12 +18,39 @@ async function PopData_Async()
 	// return the datas
 	return jsonDatas;
 }
+function GetRowColor(item)
+{
+	if (item["Infos"] == 'CMD')		return '<tr style="background-color:skyblue;">';
+	if (item["Infos"] == 'REC')		return '<tr style="background-color:red;">';
+	if (item["Infos"] == 'ABM')		return '<tr style="background-color:gray;">';
+	if (item["Genre"] == 'H')		return '<tr style="background-color:green;">';
+	return '<tr>';
+}
+function IsPopMatch(item, inputPopInfoValue)
+{
+	if ((item["Numero"].includes(inputPopInfoValue) == true) ||
+		(item["CodeBarre"].includes(inputPopInfoValue) == true) ||
+		(item["Infos"].includes(inputPopInfoValue) == true) ||
+		(item["Infos"].toUpperCase().includes(inputPopInfoValue.toUpperCase()) == true) ||
+		(item["NomComplet"].includes(inputPopInfoValue) == true) ||
+		(item["NomComplet"].toUpperCase().includes(inputPopInfoValue.toUpperCase()) == true))
+		return true;
+	return false;
+}
 
 // Creating the Results Table
-async function CreateTableWithValue(divHtmlItem, value)
+async function CreateTableWithValue(divHtmlItem, inputPopInfoValue, selectTypeSearchValue)
 {
-	let popCounter = 0;
-	
+	console.log('CreateTableWithValue(inputPopInfoValue=' + inputPopInfoValue + ', button=' + selectTypeSearchValue + ')');
+
+	// Init
+	let totalEstimation = 0.0;
+	let numberOfPopALL = 0;
+	let numberOfPopCMD = 0;
+	let numberOfPopREC = 0;
+	let numberOfPopABM = 0;
+	let numberOfPopMAS = 0;
+
 	// Loading Pop File
 	//let popDatas = PopData_NotAsync();
 	let popDatas = await PopData_Async();
@@ -31,14 +58,13 @@ async function CreateTableWithValue(divHtmlItem, value)
 	// Empty starting table
 	let str = '<br><table border="1" width="100%"><tbody>';
 
-	// <input type=text list=browsers><datalist id=browsers><option>Google<option>IE9</datalist>
-
 	// Add it to the list
 	str += '<tr style="background-color:skyblue;">';
 	str += '<td><b><font color="#000000">Numéro</b></td>';
 	str += '<td><b><font color="#000000">Code Barre</b></td>';
 	str += '<td><b><font color="#000000">Infos</b></td>';
 	str += '<td><b><font color="#000000">Estimation</b></td>';
+	str += '<td><b><font color="#000000">Etat</b></td>';
 	str += '<td><b><font color="#000000">Genre</b></td>';
 	str += '<td><b><font color="#000000">Type de Pop</b></td>';
 	str += '<td><b><font color="#000000">Nom complet</b></td>';
@@ -50,33 +76,44 @@ async function CreateTableWithValue(divHtmlItem, value)
 		// Get a Item
 		let item = popDatas[i];
 
-		// If it's the good Number
-		if ((item["Numero"].includes(value) == true) ||
-			(item["CodeBarre"].includes(value) == true) ||
-			(item["Infos"].includes(value) == true) ||
-			(item["Infos"].toUpperCase().includes(value.toUpperCase()) == true) ||
-			(item["NomComplet"].includes(value) == true) ||
-			(item["NomComplet"].toUpperCase().includes(value.toUpperCase()) == true))
-		{
-			// Color of the Row
-			if      (item["Infos"] == 'CMD') { str += '<tr style="background-color:skyblue;">';	}
-			else if (item["Infos"] == 'REC') { str += '<tr style="background-color:red;">';     }
-			else                             { str += '<tr>';                                   }
+		// Pop Counting
+		numberOfPopALL++;
+		if (item["Infos"] == 'CMD') numberOfPopCMD++;
+		if (item["Infos"] == 'REC') numberOfPopREC++;
+		if (item["Infos"] == 'ABM') numberOfPopABM++;
+		if (item["Genre"] == 'H') numberOfPopMAS++;
 
-			// Different row Informations
-			str += '<td>' + item["Numero"] + '</td>';
-			str += '<td>' + item["CodeBarre"] + '</td>';
-			str += '<td>' + item["Infos"] + '</td>';
-			str += '<td>' + item["Estimation"] + '</td>';
-			str += '<td>' + item["Genre"] + '</td>';
-			str += '<td>' + item["TypeDePop"] + '</td>';
-			str += '<td>' + item["NomComplet"] + '</td>';
-			str += '</tr>';
-			str += '</tr>';
-			
-			// One more Pop
-			popCounter++;
+		// Depends of the Select ?
+		if
+		(
+			('ALL' == selectTypeSearchValue) || 
+			(('ELL' == selectTypeSearchValue) && ((item["Infos"] == '') || (item["Infos"] == 'ABM'))) || 
+			(('MAS' == selectTypeSearchValue) && (item["Genre"] == 'H')) || 
+			(item["Infos"] == selectTypeSearchValue)
+		)
+		{
+			// Filtering the Value with the Infos
+			if (IsPopMatch(item, inputPopInfoValue) == true)
+			{
+				// Color of the Row
+				str += GetRowColor(item);
+
+				// Different row Informations
+				str += '<td>' + item["Numero"] + '</td>';
+				str += '<td>' + item["CodeBarre"] + '</td>';
+				str += '<td>' + item["Infos"] + '</td>';
+				str += '<td>' + item["Estimation"] + '</td>';
+				str += '<td>' + item["Etat"] + '</td>';
+				str += '<td>' + item["Genre"] + '</td>';
+				str += '<td>' + item["TypeDePop"] + '</td>';
+				str += '<td>' + item["NomComplet"] + '</td>';
+				str += '</tr>';
+				str += '</tr>';
+			}
 		}
+
+		// Computation Estimation
+		if (item["Estimation"] != 'XXXXXX') totalEstimation += (parseFloat(item["Estimation"] * 100) / 100);
 	}
 
 	// Add it to the list
@@ -90,19 +127,49 @@ async function CreateTableWithValue(divHtmlItem, value)
 	// Send it to the HTML of the DIV
 	document.getElementById(divHtmlItem).innerHTML = str;
 
-	// Update the number of Pops
-	document.getElementById("numberOfPops").innerHTML = 'Liste des Pops (' + popCounter + '/' + popDatas.length + ')';
+	// Update the Select Options
+	document.getElementById("selectTypeSearchALL").text = 'Toutes les Pops (' + numberOfPopALL + ')';
+	document.getElementById("selectTypeSearchELL").text = 'Pops Possédées (' + (numberOfPopALL-numberOfPopREC-numberOfPopCMD) + ')';
+	document.getElementById("selectTypeSearchCMD").text = 'Pops Commandées (' + numberOfPopCMD + ')';
+	document.getElementById("selectTypeSearchREC").text = 'Pops Recherchées (' + numberOfPopREC + ')';
+	document.getElementById("selectTypeSearchABM").text = 'Pops Abimées (' + numberOfPopABM + ')';
+	document.getElementById("selectTypeSearchMAS").text = 'Pops Masculines (' + numberOfPopMAS + ')';
+
+	// Update the Version Estimation
+	document.getElementById("versionOfPops").innerHTML = 'Version 0.9.0';
+
+	// Console Estimation Infos
+	console.log('Estimation Totale = ' + totalEstimation + ' Euros');
 }
 
-// Keypress event ? 
+// Keypress event in the Input ?
 document.getElementById('inputPopInfo').addEventListener("keyup", function(event)
 {
 	// Get the value of the Input
-	let value = document.getElementById('inputPopInfo').value;
+	let inputPopInfoValue = document.getElementById('inputPopInfo').value;
+
+	// Get the value of the Select
+	let selectTypeSearchValue = document.getElementById('selectTypeSearch').value;
 
 	// Create the table with the infos
-	CreateTableWithValue('divall', value);
+	CreateTableWithValue('divall', inputPopInfoValue, selectTypeSearchValue);
 });
+
+// Select choice change ?
+document.getElementById('selectTypeSearch').addEventListener("change", function(event)
+{
+	// Get the value of the Input
+	let inputPopInfoValue = document.getElementById('inputPopInfo').value;
+
+	// Get the value of the Select
+	let selectTypeSearchValue = document.getElementById('selectTypeSearch').value;
+
+	// Create the table with the infos
+	CreateTableWithValue('divall', inputPopInfoValue, selectTypeSearchValue);
+});
+
+// Full Refresh
+CreateTableWithValue('divall', '', 'ALL');
 
 // Focus on the Input
 document.getElementById('inputPopInfo').focus();
