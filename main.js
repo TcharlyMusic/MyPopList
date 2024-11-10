4607ec4d60a42ef0cb62e29b2366efb3088d0d50
TcharlyMusic <duprezjc@gmail.com>
2024-11-10 18:23:44 +0100

    Update DATAS.JSON with Licenses

--- main.js
+++ main.js
@@ -38,10 +38,86 @@ function IsPopMatch(item, inputPopInfoValue)
 	return false;
 }
 
-// Creating the Results Table
-async function CreateTableWithValue(divHtmlItem, inputPopInfoValue, selectTypeSearchValue)
+async function CreateSelectTypeSearch()
 {
-	console.log('CreateTableWithValue(inputPopInfoValue=' + inputPopInfoValue + ', button=' + selectTypeSearchValue + ')');
+	// Starting the HTML section Select
+	let str = '<select id="selectTypeSearch">';
+
+	// TypeSearch Options with Ids
+	str += '<option id="selectTypeSearchALL" value="ALL">Toutes les Pops</option>';
+	str += '<option id="selectTypeSearchELL" value="ELL">Pops Présentées</option>';
+	str += '<option id="selectTypeSearchCMD" value="CMD">Pops Commandées</option>';
+	str += '<option id="selectTypeSearchREC" value="REC">Pops Recherchées</option>';
+	str += '<option id="selectTypeSearchABM" value="ABM">Pops Abimées</option>';
+	str += '<option id="selectTypeSearchMAS" value="HOM">Pops Masculines</option>';
+
+	// Ending the HTML section Select
+	str += '</select>';
+
+	// Send the Generated Select to the HTML of the DIV
+	document.getElementById('divTypeSearchResult').innerHTML = str;
+}
+function ObjectAlreadyInList(list, str)
+{
+	for (let i = 0; i < list.length; i++)
+	{
+		if (list[i] == str) return true;
+	}
+	return false;
+}
+
+async function CreateSelectLicenses()
+{
+	// Empty list of Licenses
+	let licenseList = new Array();
+
+	// Get the Pop List
+	let popDatas = await PopData_Async();
+
+	// loop on all the item of the Pop List
+	for (let i = 0; i < popDatas.length; i++)
+	{
+		// Get a Item
+		let item = popDatas[i];
+
+		// If the License exist
+		if (item["License"] != '')
+		{
+			// If the License is not in the List
+			if (ObjectAlreadyInList(licenseList, item["License"]) == false)
+			{
+				// Add it
+				licenseList.push(item["License"]);
+			}				
+		}
+	}
+	
+	// Ordering by alphabetic
+	licenseList.sort();
+	
+	// Starting the HTML section Select
+	let str = '<select id="selectLicense">';
+
+	// 1st Option : ALL
+	str += '<option value="ALL">Toutes les Licenses</option>';
+
+	// loop on all the Licenses found
+	for (let i = 0; i < licenseList.length; i++)
+	{
+		// Generate the License Select
+		str += '<option value="' + licenseList[i] + '">' + licenseList[i] + '</option>';
+	}
+	
+	// Ending the HTML section Select
+	str += '</select>';
+
+	// Send the Generated Select to the HTML of the DIV
+	document.getElementById('divLicenseResult').innerHTML = str;
+}
+
+async function CreateTableResults(inputPopInfoValue, selectTypeSearchValue, selectLicenseValue)
+{
+	console.log('CreateTableResults(inputPopInfoValue=' + inputPopInfoValue + ', TypeSearch=' + selectTypeSearchValue + ', License=' + selectLicenseValue + ')');
 
 	// Init
 	let totalEstimation = 0.0;
@@ -55,7 +131,7 @@ async function CreateTableWithValue(divHtmlItem, inputPopInfoValue, selectTypeSe
 	//let popDatas = PopData_NotAsync();
 	let popDatas = await PopData_Async();
 
-	// Empty starting table
+	// Starting the HTML section Table
 	let str = '<br><table border="1" width="100%"><tbody>';
 
 	// Add it to the list
@@ -67,6 +143,7 @@ async function CreateTableWithValue(divHtmlItem, inputPopInfoValue, selectTypeSe
 	str += '<td><b><font color="#000000">Etat</b></td>';
 	str += '<td><b><font color="#000000">Genre</b></td>';
 	str += '<td><b><font color="#000000">Type de Pop</b></td>';
+	str += '<td><b><font color="#000000">License</b></td>';
 	str += '<td><b><font color="#000000">Nom complet</b></td>';
 	str += '</tr>';
 
@@ -88,39 +165,37 @@ async function CreateTableWithValue(divHtmlItem, inputPopInfoValue, selectTypeSe
 		(
 			('ALL' == selectTypeSearchValue) || 
 			(('ELL' == selectTypeSearchValue) && ((item["Infos"] == '') || (item["Infos"] == 'ABM'))) || 
-			(('MAS' == selectTypeSearchValue) && (item["Genre"] == 'H')) || 
+			(('HOM' == selectTypeSearchValue) && (item["Genre"] == 'H')) || 
 			(item["Infos"] == selectTypeSearchValue)
 		)
 		{
 			// Filtering the Value with the Infos
 			if (IsPopMatch(item, inputPopInfoValue) == true)
 			{
-				// Color of the Row
-				str += GetRowColor(item);
+				// Good License ?
+				if (('ALL' == selectLicenseValue) || (item["License"] == selectLicenseValue))
+				{
+					// Color of the Row
+					str += GetRowColor(item);
 
-				// Different row Informations
-				str += '<td>' + item["Numero"] + '</td>';
-				str += '<td>' + item["CodeBarre"] + '</td>';
-				str += '<td>' + item["Infos"] + '</td>';
-				str += '<td>' + item["Estimation"] + '</td>';
-				str += '<td>' + item["Etat"] + '</td>';
-				str += '<td>' + item["Genre"] + '</td>';
-				str += '<td>' + item["TypeDePop"] + '</td>';
-				str += '<td>' + item["NomComplet"] + '</td>';
-				str += '</tr>';
-				str += '</tr>';
+					// Different row Informations
+					str += '<td>' + item["Numero"] + '</td>';
+					str += '<td>' + item["CodeBarre"] + '</td>';
+					str += '<td>' + item["Infos"] + '</td>';
+					str += '<td>' + item["Estimation"] + '</td>';
+					str += '<td>' + item["Etat"] + '</td>';
+					str += '<td>' + item["Genre"] + '</td>';
+					str += '<td>' + item["TypeDePop"] + '</td>';
+					str += '<td>' + item["License"] + '</td>';
+					str += '<td>' + item["NomComplet"] + '</td>';
+					str += '</tr>';
+					str += '</tr>';
+				}
 			}
 		}
 
 		// Computation Estimation
-		if (item["Estimation"] != 'XXXXXX')
-		{
-			// If it's not a searched Pop
-			if (item["Infos"] != 'REC')
-			{
-				totalEstimation += (parseFloat(item["Estimation"] * 100) / 100);
-			}
-		}
+		if (item["Estimation"] != 'XXXXXX') { if (item["Infos"] != 'REC') { totalEstimation += (parseFloat(item["Estimation"] * 100) / 100); }}
 	}
 	
 	// Add it to the list
@@ -128,11 +203,11 @@ async function CreateTableWithValue(divHtmlItem, inputPopInfoValue, selectTypeSe
 	//str += '<td><img src="/ViteProject/images/60100.webp" width="32" height="32" /></td>';
 	//str += '</tr>';
 
-	// End of the Table
+	// Ending the HTML section Table
 	str += '</tbody></table>';
 
-	// Send it to the HTML of the DIV
-	document.getElementById(divHtmlItem).innerHTML = str;
+	// Send the generated Table to the HTML of the DIV
+	document.getElementById('divTableReult').innerHTML = str;
 
 	// Update the Select Options
 	document.getElementById("selectTypeSearchALL").text = 'Toutes les Pops (' + numberOfPopALL + ')';
@@ -142,41 +217,47 @@ async function CreateTableWithValue(divHtmlItem, inputPopInfoValue, selectTypeSe
 	document.getElementById("selectTypeSearchABM").text = 'Pops Abimées (' + numberOfPopABM + ')';
 	document.getElementById("selectTypeSearchMAS").text = 'Pops Masculines (' + numberOfPopMAS + ')';
 
-	// Update the Version Estimation
-	document.getElementById("versionOfPops").innerHTML = 'Version 0.9.0';
-
 	// Console Estimation Infos
 	console.log('Estimation Totale = ' + totalEstimation + ' Euros');
 }
 
-// Keypress event in the Input ?
-document.getElementById('inputPopInfo').addEventListener("keyup", function(event)
+async function FullRefresh()
 {
 	// Get the value of the Input
 	let inputPopInfoValue = document.getElementById('inputPopInfo').value;
 
-	// Get the value of the Select
+	// Get the value of the Select Type
 	let selectTypeSearchValue = document.getElementById('selectTypeSearch').value;
 
-	// Create the table with the infos
-	CreateTableWithValue('divall', inputPopInfoValue, selectTypeSearchValue);
-});
+	// Get the value of the Select License
+	let selectLicenseValue = document.getElementById('selectLicense').value;
 
-// Select choice change ?
-document.getElementById('selectTypeSearch').addEventListener("change", function(event)
+	// Create the table with the infos
+	CreateTableResults(inputPopInfoValue, selectTypeSearchValue, selectLicenseValue);	
+}
+
+async function main()
 {
-	// Get the value of the Input
-	let inputPopInfoValue = document.getElementById('inputPopInfo').value;
+	// Create the Select Type Search
+	await CreateSelectTypeSearch();
+	
+	// Create the Select License
+	await CreateSelectLicenses();
+	
+	// Events
+	document.getElementById('selectTypeSearch').addEventListener("change", FullRefresh);
+	document.getElementById('selectLicense').addEventListener("change", FullRefresh);
+	document.getElementById('inputPopInfo').addEventListener("keyup", FullRefresh);
 
-	// Get the value of the Select
-	let selectTypeSearchValue = document.getElementById('selectTypeSearch').value;
+	// Create the Table result control
+	await CreateTableResults('', 'ALL', 'ALL');
 
-	// Create the table with the infos
-	CreateTableWithValue('divall', inputPopInfoValue, selectTypeSearchValue);
-});
+	// Focus on the Input
+	document.getElementById('inputPopInfo').focus();
 
-// Full Refresh
-CreateTableWithValue('divall', '', 'ALL');
+	// Update the Version Estimation
+	document.getElementById("versionOfPops").innerHTML = 'Version 0.9.1';
+}
 
-// Focus on the Input
-document.getElementById('inputPopInfo').focus();
+// Main Function
+main();
